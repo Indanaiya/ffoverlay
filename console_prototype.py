e906033efa2360146f8683cfcbd3a9a5a4aa0fdf
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-06-16 15:56:12 +0100

    Add some error checking for HTTP request and file opening

--- console_prototype.py
+++ console_prototype.py
@@ -3,10 +3,15 @@ import json
 
 baseurl = "https://universalis.app/api/Chaos/"
 
-#Should change this to a try catch to display custom messages if something goes wrong
-with open('values.json') as file:
-    valuesData = json.load(file)
-    for key in list(valuesData.keys()):
-        with requests.request("GET", "https://universalis.app/api/Chaos/" + valuesData[key]["id"]) as response:
-            responseJson = response.json()
-            print("Item Id: " + str(responseJson['itemID']) + ", lowest price: " + str(responseJson['listings'][0]['pricePerUnit']))
+try:
+    with open('values.json') as file:
+        valuesData = json.load(file)
+        for key in list(valuesData.keys()):
+            try:
+                with requests.request("GET", baseurl + valuesData[key]["id"]) as response:
+                    responseJson = response.json()
+                    print("Item Id: " + str(responseJson['itemID']) + ", lowest price: " + str(responseJson['listings'][0]['pricePerUnit']))
+            except requests.exceptions.RequestException as err:
+                print(f"Unable to get data from Universalis for {key}: {repr(err)}\n")
+except Exception as err:
+    print(repr(err))
