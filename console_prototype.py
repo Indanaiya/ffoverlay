7f8ec555777d23b507234ef3ebf5b8a13fb443e3
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-06-16 17:21:52 +0100

    Add __init__.py Add method to get eorzea time as a decimal

--- console_prototype.py
+++ console_prototype.py
@@ -1,8 +1,19 @@
 import requests
 import json
+import functools
+import asyncio
+from eorzea_time import getEorzeaTime
 
 baseurl = "https://universalis.app/api/Chaos/"
 
+loop = asyncio.get_event_loop()
+
+async def gatherAlert(key, valuesData, price):
+    """Prints a message when a new gathering node spawns"""
+    print(f"New node spawn: {valuesData[key][name]}  {price}gil per unit")
+
+#This will need to be async in the final product:
+#aiohttp?
 try:
     with open('values.json') as file:
         valuesData = json.load(file)
@@ -11,7 +22,13 @@ try:
                 with requests.request("GET", baseurl + valuesData[key]["id"]) as response:
                     responseJson = response.json()
                     print("Item Id: " + str(responseJson['itemID']) + ", lowest price: " + str(responseJson['listings'][0]['pricePerUnit']))
+                    loop
             except requests.exceptions.RequestException as err:
                 print(f"Unable to get data from Universalis for {key}: {repr(err)}\n")
 except Exception as err:
     print(repr(err))
+
+
+
+
+print(getEorzeaTime())
--- eorzea_time.py
+++ eorzea_time.py
@@ -1,12 +1,37 @@
 import datetime
 import time
 
+eorzeaEpochMultiplier = 20571.428571428573
+
+def getEorzeaTimeDecimal():
+        """
+        Created using code provided by /u/Clorifex https://www.reddit.com/r/ffxiv/comments/2pbl8p/eorzea_time_formula/cmvijkz?utm_source=share&utm_medium=web2x
+        Returns a tuple of the type (float, float), representing hours and minutes in eorzean time (On EU servers anyway)
+        """
+        localEpoch = time.time()
+        eorzeaEpoch = localEpoch * eorzeaEpochMultiplier
+        eorzeaMinutes = (eorzeaEpoch / (1000 * 60)) % 60
+        eorzeaHours = (eorzeaEpoch / (1000 * 60 * 60)) % 24
+        return (eorzeaHours, eorzeaMinutes)
+
 def getEorzeaTime():
-    """Created using code provided by /u/Clorifex https://www.reddit.com/r/ffxiv/comments/2pbl8p/eorzea_time_formula/cmvijkz?utm_source=share&utm_medium=web2x
+    """
     Returns a tuple of the type (int, int), representing hours and minutes in eorzean time (On EU servers anyway)
     """
+    eorzeaHoursDecimal, eorzeaMinutesDecimal = getEorzeaTimeDecimal()
+    return (int(eorzeaHoursDecimal), int(eorzeaMinutesDecimal))
+
+def timeUntilInEorzea(targetTime):
+    """
+    Target time is in hours
+    """
+    #Need error checking to make sure that eorzeaHours is a number between 0 and 24 (not including 24)
     localEpoch = time.time()
-    eorzeaEpoch = localEpoch * 20571.428571428573
-    eorzeaMinutes = int((eorzeaEpoch / (1000 * 60)) % 60)
-    eorzeaHours = int((eorzeaEpoch / (1000 * 60 * 60)) % 24)
-    return (eorzeaHours, eorzeaMinutes)
+    eorzeaHours, eorzeaMinutes = getEorzeaTimeDecimal()
+    n = 1
+    return (((24*n+eorzeaHours)*1000*60*60)/eorzeaEpochMultiplier)
+
+if __name__ == "__main__":
+    print(getEorzeaTimeDecimal())
+    print(getEorzeaTime())
+    print(timeUntilInEorzea(14))
