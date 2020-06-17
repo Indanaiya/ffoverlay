58a2f13870b978803c5fd8375f0577f1f340839b
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-06-17 01:30:36 +0100

    Add custom behaviour defined by callback functions when a node spawns and despawns

--- src/notifications_provider.py
+++ src/notifications_provider.py
@@ -28,22 +28,46 @@ class NotificationsProvider:
             return gatheredItemsData, marketData
 
 
-    def __init__(self, gatheredItemsLocation, marketDataAddress):
+    def __init__(self, gatheredItemsLocation, marketDataAddress, spawnCallback, despawnCallback):
+        if not 'name' in spawnCallback.__code__.co_varnames and not 'price' in spawnCallback.__code__.co_varnames:
+            raise ValueError("Expected name and price parameters in spawnCallback")
+        if not 'name' in despawnCallback.__code__.co_varnames:
+            raise ValueError("Expected name parameter in despawnCallback")
+
         self.gatheredItemsData, self.marketData = self.getData(gatheredItemsLocation, marketDataAddress)
+        self.spawnCallback = spawnCallback
+        self.despawnCallback = despawnCallback
 
     async def gatherAlert(self, key):
-        """Prints a message when a new gathering node spawns"""
+        """
+        Executes the self.spawnCallback function when the node named 'key' spawns
+        Executes the self.despawnCallback function when the node named 'key' despawns
+        self.spawnCallback should have the arguments name and price
+        self.despawnCallback should have the argument name
+        """
         valuesData = self.gatheredItemsData
         price = self.marketData[key]['listings'][0]['pricePerUnit']
         while True:
             eorzeaHours, eorzeaMinutes = getEorzeaTimeDecimal()
+            currentTimeIndex = 0
             nextTimeIndex = 0
+            name = valuesData[key]['name']
             spawnTimes = valuesData[key]['spawnTimes']
             for i in range(len(spawnTimes)):
                 #print(f"Node: {key}, i: {i}")
-                if eorzeaHours >= int(spawnTimes[i][:2]) and eorzeaHours < int(spawnTimes[i][:2])+valuesData[key]['lifespan']:
-                    print(f"New node spawn[{eorzeaHours}]: {valuesData[key]['name']}  {price}gil per unit")
+                if eorzeaHours >= int(spawnTimes[i][:2]) and eorzeaHours < int(spawnTimes[i][:2])+valuesData[key]['lifespan']: #Means the node is up
+                    #print(f"New node spawn[{eorzeaHours}]: {valuesData[key]['name']}  {price}gil per unit")
+                    currentTimeIndex = i
                     nextTimeIndex = (i+1, 0)[i==len(spawnTimes)-1]
+
+                    #Notification for spawn:
+                    self.spawnCallback(name=name, price=price)
+
+                    #Notification for despawn:
+                    sleepTime = timeUntilInEorzea(int(spawnTimes[currentTimeIndex][:2]) + self.gatheredItemsData[key]['lifespan'])
+                    await asyncio.sleep(sleepTime)
+                    self.despawnCallback(name=name)
+
                     break
                 elif eorzeaHours > int(spawnTimes[i][:2]):
                     if i == len(spawnTimes)-1:
@@ -52,33 +76,31 @@ class NotificationsProvider:
                     nextTimeIndex = i
                     break
 
+            #Wait for node to spawn again:
             print(f"Node: {key}, nextTimeIndex: {nextTimeIndex}")
             sleepTime = timeUntilInEorzea(int(spawnTimes[nextTimeIndex][:2]))
-            # print(f"Sleeping for: {sleepTime}")
             await asyncio.sleep(sleepTime)
 
     def beginGatherAlerts(self):
+        """
+        Starts gatherAlerts for all gatherable items on a new event loop
+        """
         try:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            functions = []
-            for key in list(self.gatheredItemsData.keys()):
-                functions.append(self.gatherAlert(key))
+            functions = [self.gatherAlert(key) for key in list(self.gatheredItemsData.keys())]
             loop.run_until_complete(asyncio.gather(*functions))
-            # loop.run_until_complete(asyncio.gather(
-            #     self.gatherAlert('Imperial Fern'),
-            #     self.gatherAlert('Fireheart Cobalt'),
-            #     self.gatherAlert('Duskblooms'),
-            #     self.gatherAlert('Purpure Shell Chips'),
-            #     self.gatherAlert('Merbau Log'),
-            #     self.gatherAlert('Ashen Alumen'),
-            #     ))
-
         finally:
             loop.close()
 
+def printSpawnMessage(name=None, price=None):
+    print(f"New node spawn[{getEorzeaTime()}]: {name}  {price}gil per unit")
+
+def printDespawnMessage(name=None):
+    print(f"Node despawn[{getEorzeaTime()}]: {name}")
+
 if __name__ == "__main__":
-    notificationsProvider = NotificationsProvider('../res/values.json', "https://universalis.app/api/Chaos/")
+    notificationsProvider = NotificationsProvider('../res/values.json', "https://universalis.app/api/Chaos/", printSpawnMessage, printDespawnMessage)
     x=threading.Thread(target=notificationsProvider.beginGatherAlerts)
     x.start()
     #notificationsProvider.beginGatherAlerts()
