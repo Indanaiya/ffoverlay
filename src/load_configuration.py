1c6e4448228eaa4556811da017cf8b18e0943698
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-06-22 17:44:15 +0100

    Add datacenter option to config

--- src/load_configuration.py
+++ src/load_configuration.py
@@ -16,6 +16,12 @@ def getConfig():
         generateConfig()
     parser = configparser.ConfigParser()
     parser.read(configAddress)
+    #Checks that all expected options are found in config.ini, rewrites the file if not:
+    if not all(x in parser._sections.keys() for x in defaults.keys()):
+        generateConfig()
+        parser = configparser.ConfigParser()
+        parser.read(configAddress)
+        
     return parser._sections
 
 def updateValue(key, value):
@@ -23,7 +29,7 @@ def updateValue(key, value):
         raise KeyError(f"Invalid Key. {key} is not recognised.")
     if not value in acceptedValues[key]:
         raise ValueError(f"{key} cannot take value {value}. Accepted values are: {acceptedValues[key]}")
-    #Get the config file as it exists now:
+    #Get the config file:
     parser = configparser.ConfigParser()
     parser.read(configAddress)
     #Update the value in python:
